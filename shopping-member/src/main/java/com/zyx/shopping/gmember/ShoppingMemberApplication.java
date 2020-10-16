73af98754959b11381b37cf0c2c7d97e842a0058
Zhaoyx <Zhaoyunxin0922@outlook.com>
2020-10-16 21:41:04 +0800

    项目初始结构创建

---
+++ shopping-member/src/main/java/com/zyx/shopping/gmember/ShoppingMemberApplication.java
@@ -0,0 +1,13 @@
+package com.zyx.shopping.gmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMemberApplication.class, args);
+	}
+
+}
---
+++ shopping-member/src/test/java/com/zyx/shopping/gmember/ShoppingMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.shopping.gmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
